6e23010707e7f42f60b07f789cee9d4f30d921b3
s4Kc0d3 <44966167+s4Kc0d3@users.noreply.github.com>
2018-11-12 10:43:49 +0100

    Add files via upload

---
+++ LogoffUsersDisconnect.ps1
@@ -0,0 +1,25 @@
+#Script that disconnects inactive users on system carrying more than 48 h.
+#Created By: Isaac Iborra (iiborra@externos.itnow.es)
+
+$usersDisc = quser | select-string "Disc" | select-string -notmatch "services"
+$today = (get-date).tostring('dd/MM/yyyy')
+$yesterday = (get-date).addDays(-1).tostring('dd/MM/yyyy')
+
+if ($usersDisc){
+	$usersDisc | % {
+		$split = ($_.tostring() -split ' +')
+		$user = $split[1]
+		$date = $split[5]
+     
+		if ($today -eq $date){
+			Write-Host "User: $user is inactive today: $date"			
+		}elseif ($yesterday -eq $date){
+            write-host "User: $user is inactive since yesterday: $date"
+        }else{
+            Write-Host "We proceed to disconnecting the user: $user dated $date by being inactive for more than 48 h"
+            logoff ($_.tostring() -split ' +')[2]
+        }
+	}
+}else{
+	Write-Host "There are no inactive users in the system"
+}
