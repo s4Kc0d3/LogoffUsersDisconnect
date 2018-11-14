c0bcc94f8a591a57208f7d5b891aad5a232f1d03
s4Kc0d3 <44966167+s4Kc0d3@users.noreply.github.com>
2018-11-14 15:17:52 +0100

    Update LogoffUsersDisconnect.ps1

--- LogoffUsersDisconnect.ps1
+++ LogoffUsersDisconnect.ps1
@@ -5,39 +5,38 @@
 Get-ChildItem -Path "D:\scripts\Controlsesiones\Logs" -Recurse | Where-Object CreationTime -LT (Get-Date).AddDays(-30) | Remove-Item 
 $usersAll = quser
 $usersDisc = quser | select-string "Disc" | select-string -notmatch "services"
-$today = (get-date).tostring('MM/dd/yyyy')
 $now = (get-date).tostring('MM_dd_yyyy')
-$yesterday = (get-date).addDays(-1).tostring('MM/dd/yyyy')
 $results = @()
 $results += $usersAll
-$rutaDelArchivo = "D:\scripts\Controlsesiones\Logs\MonitDisconnected_"+$now+".txt"
+$currentfolder = Split-Path -parent $PSCommandPath 
+$rutaDelArchivo = $currentfolder + "\Logs\MonitDisconnected_"+$now+".txt"
 
 if ($usersDisc){
 	$usersDisc | % {
 		$split = ($_.tostring() -split ' +')
 		$user = $split[1]
-		$date = $split[5]
-		Write-Host "$yesterday - $today - $date"
-		if ($today -eq $date){
-			$comments = "User: $user is inactive today: $date"
+		$date = $split[4]
+		if ($date.indexof("+") -ne -1){
+			$days = $date.split("+")
+			if ($days -contains "1" -or $days -contains "2") {
+				$comments = "user: $user dated $date is Disconnect to $days Days." 
+				Write-Host $comments
+				$results += $comments
+			}else{
+				$comments = "Proceed to disconnecting the user: $user dated $date for $days to Disconnect"
+				Write-Host $comments
+				$results += $comments
+			}
+		} else {
+			$comments = "The user $user idle time to disconnect is $date"
 			Write-Host $comments
-			$results += $comments
-			
-		}elseif ($yesterday -eq $date){
-			$comments = "User: $user is inactive since yesterday: $date"
-            write-host $comments
-			$results += $comments
-        }else{
-			$comments = "We proceed to disconnecting the user: $user dated $date by being inactive for more than 48 h"
-            Write-Host $comments
-			$results += $comments
-            logoff ($_.tostring() -split ' +')[2]
-        }
+		}
 	}
+
 }else{
 	$comments = "There are no inactive users in the system"
 	Write-Host $comments
 	$results += $comments
 }
-Write-Host "$yesterday - $today - $date"
+
 $results| Out-File -FilePath $rutaDelArchivo
